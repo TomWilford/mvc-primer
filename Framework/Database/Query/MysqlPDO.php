f4759b5707d86aa464271952c9a85ffc51ff51b0
Tom Wilford <hello@jollyblueman.com>
2021-03-06 23:11:43 +0000

    Comments

--- Framework/Database/Query/MysqlPDO.php
+++ Framework/Database/Query/MysqlPDO.php
@@ -4,6 +4,7 @@ namespace Framework\Database\Query;
 
 use Framework\Database;
 use Framework\Database\Exception;
+use PDOStatement;
 
 /**
  * @property Database\Connector\MysqlPDO connector
@@ -88,6 +89,13 @@ class MysqlPDO extends Database\Query
      */
     protected array $_values = [];
 
+    /**
+     * Prepares &&|| runs query from string and array of arguments
+     * @param $sql
+     * @param array $arguments
+     * @return array|false|PDOStatement
+     * @throws Exception\Service
+     */
     public function string($sql, $arguments = [])
     {
         if (!$arguments)
@@ -97,6 +105,142 @@ class MysqlPDO extends Database\Query
         return $this->connector->prepareAndExecute($sql, $arguments);
     }
 
+    /**
+     * Simple Query Generator:
+     * - Sets generator variables for select query
+     * - Defaults to select all
+     * @param $from
+     * @param string[] $fields
+     * @return $this
+     * @throws Exception\Argument
+     */
+    public function select($from, $fields = ["*"])
+    {
+        if (empty($from))
+        {
+            throw new Exception\Argument("Invalid argument");
+        }
+
+        $this->_table   = $from;
+        $this->_clause = "SELECT";
+        $this->_fields = $fields;
+
+        return $this;
+    }
+
+    /**
+     * Simple Query Generator:
+     * - Sets generator variables for select query with a limit of 1
+     * - Defaults to select all
+     * @param $from
+     * @param string[] $fields
+     * @return $this
+     * @throws Exception\Argument
+     */
+    public function selectFirst($from, $fields = ["*"])
+    {
+        $this->select($from, $fields);
+        $this->limit(1);
+
+        return $this;
+    }
+
+    /**
+     * Simple Query Generator:
+     * - Sets generator variables for select count query
+     * @param $from
+     * @return $this
+     * @throws Exception\Argument
+     */
+    public function countAll($from)
+    {
+        if (empty($from))
+        {
+            throw new Exception\Argument("Invalid argument");
+        }
+
+        $this->_table   = $from;
+        $this->_clause = "SELECT";
+        $this->_fields = ["COUNT(*) AS total"];
+
+        return $this;
+    }
+
+    /**
+     * Simple Query Generator:
+     * - Sets generator variables for insert query
+     * - Sorts arguments into fields & values
+     * @param $into
+     * @param $arguments
+     * @return $this
+     * @throws Exception\Argument
+     */
+    public function insert($into, $arguments)
+    {
+        if (empty($into) || empty($arguments))
+        {
+            throw new Exception\Argument("Invalid argument");
+        }
+
+        $this->_table  = $into;
+        $this->_clause = "INSERT";
+        $this->_processArguments($arguments);
+
+        return $this;
+    }
+
+    /**
+     * Simple Query Generator:
+     * - Sets generator variables for delete query
+     * @param $from
+     * @return $this
+     * @throws Exception\Argument
+     */
+    public function delete($from)
+    {
+        if (empty($from))
+        {
+            throw new Exception\Argument("Invalid argument");
+        }
+
+        $this->_table   = $from;
+        $this->_clause = "DELETE";
+
+        return $this;
+    }
+
+    /**
+     * Simple Query Generator:
+     * - Sets generator variables for update query
+     * - Sorts arguments into fields & values
+     * @param $table
+     * @param $arguments
+     * @return $this
+     * @throws Exception\Argument
+     */
+    public function update($table, $arguments)
+    {
+        if (empty($table) || empty($arguments))
+        {
+            throw new Exception\Argument("Invalid argument");
+        }
+
+        $this->_table  = $table;
+        $this->_clause = "UPDATE";
+        $this->_processArguments($arguments);
+
+        return $this;
+    }
+
+    /**
+     * Simple Query Generator:
+     * - Sets generator variables for joins in query
+     * @param $join
+     * @param $on
+     * @param array $fields
+     * @return $this
+     * @throws Exception\Argument
+     */
     public function join($join, $on, $fields = [])
     {
         if (empty($join))
@@ -117,6 +261,14 @@ class MysqlPDO extends Database\Query
         return $this;
     }
 
+    /**
+     * Simple Query Generator:
+     * - Sets generator variables to limit query
+     * @param $limit
+     * @param int $page
+     * @return $this
+     * @throws Exception\Argument
+     */
     public function limit($limit, $page = 1)
     {
         if (empty($limit))
@@ -130,6 +282,14 @@ class MysqlPDO extends Database\Query
         return $this;
     }
 
+    /**
+     * Simple Query Generator:
+     * - Sets generator variables for ordering in query
+     * @param $order
+     * @param string $direction
+     * @return $this
+     * @throws Exception\Argument
+     */
     public function order($order, $direction = "asc")
     {
         if (empty($order))
@@ -143,6 +303,12 @@ class MysqlPDO extends Database\Query
         return $this;
     }
 
+    /**
+     * Simple Query Generator:
+     * - Create array of where clauses for query
+     * @return $this
+     * @throws Exception\Argument
+     */
     public function where()
     {
         $arguments   = func_get_args();
@@ -158,83 +324,11 @@ class MysqlPDO extends Database\Query
         return $this;
     }
 
-    public function select($from, $fields = ["*"])
-    {
-        if (empty($from))
-        {
-            throw new Exception\Argument("Invalid argument");
-        }
-
-        $this->_table   = $from;
-        $this->_clause = "SELECT";
-        $this->_fields = $fields;
-
-        return $this;
-    }
-
-    public function selectFirst($from, $fields = ["*"])
-    {
-        $this->select($from, $fields);
-        $this->limit(1);
-
-        return $this;
-    }
-
-    public function countAll($from)
-    {
-        if (empty($from))
-        {
-            throw new Exception\Argument("Invalid argument");
-        }
-
-        $this->_table   = $from;
-        $this->_clause = "SELECT";
-        $this->_fields = ["COUNT(*) AS total"];
-
-        return $this;
-    }
-
-    public function insert($into, $arguments)
-    {
-        if (empty($into) || empty($arguments))
-        {
-            throw new Exception\Argument("Invalid argument");
-        }
-
-        $this->_table  = $into;
-        $this->_clause = "INSERT";
-        $this->_processArguments($arguments);
-
-        return $this;
-    }
-
-    public function delete($from)
-    {
-        if (empty($from))
-        {
-            throw new Exception\Argument("Invalid argument");
-        }
-
-        $this->_table   = $from;
-        $this->_clause = "DELETE";
-
-        return $this;
-    }
-
-    public function update($table, $arguments)
-    {
-        if (empty($table) || empty($arguments))
-        {
-            throw new Exception\Argument("Invalid argument");
-        }
-
-        $this->_table  = $table;
-        $this->_clause = "UPDATE";
-        $this->_processArguments($arguments);
-
-        return $this;
-    }
-
+    /**
+     * Simple Query Generator:
+     * - Processes arguments for update/insert queries
+     * @param $arguments
+     */
     private function _processArguments($arguments)
     {
         foreach ($arguments as $field => $value)
@@ -244,12 +338,20 @@ class MysqlPDO extends Database\Query
         }
     }
 
+    /**
+     * Simple Query Generator:
+     * - Finds the right sql builder by the clause
+     * - Prepares &&|| runs query, returning statement
+     * @return array|false|PDOStatement
+     * @throws Exception\Argument
+     * @throws Exception\Service
+     */
     public function run()
     {
         switch ($this->_clause)
         {
             case "SELECT":
-                    return $this->string($this->_buildSelect(), $this->_arguments);
+                return $this->string($this->_buildSelect(), $this->_arguments);
                 break;
             case "INSERT":
                 return $this->string($this->_buildInsert(), $this->_arguments);
@@ -265,6 +367,11 @@ class MysqlPDO extends Database\Query
         }
     }
 
+    /**
+     * Simple Query Generator:
+     * - Builds select statement from relevant variables
+     * @return string
+     */
     private function _buildSelect()
     {
         $select = $this->_clause;
@@ -288,6 +395,11 @@ class MysqlPDO extends Database\Query
         return "{$select} {$fields} {$from} {$join} {$where} {$order} {$limit}";
     }
 
+    /**
+     * Simple Query Generator:
+     * - Builds insert statement from relevant variables
+     * @return string
+     */
     private function _buildInsert()
     {
         $insertInto = $this->_clause . " INTO";
@@ -306,6 +418,11 @@ class MysqlPDO extends Database\Query
         return "{$insertInto} {$table} ({$fields}) VALUES ({$values})";
     }
 
+    /**
+     * Simple Query Generator:
+     * - Builds update statement from relevant variables
+     * @return string
+     */
     private function _buildUpdate()
     {
         $update = $this->_clause;
@@ -332,6 +449,11 @@ class MysqlPDO extends Database\Query
         return "{$update} {$table} SET {$fields} {$where} {$limit}";
     }
 
+    /**
+     * Simple Query Generator:
+     * - Builds delete statement from relevant variables
+     * @return string
+     */
     private function _buildDelete()
     {
         $delete = $this->_clause;
@@ -345,6 +467,12 @@ class MysqlPDO extends Database\Query
         return "{$delete} FROM {$table} {$where} {$limit}";
     }
 
+    /**
+     * Simple Query Generator:
+     * - Creates sql for WHERE from all where arguments
+     * @param $where
+     * @return string
+     */
     private function _buildWhere($where)
     {
 
@@ -352,6 +480,13 @@ class MysqlPDO extends Database\Query
         return ($_where) ? "WHERE {$_where}" : "";
     }
 
+    /**
+     * Simple Query Generator:
+     * - Creates sql for LIMIT
+     * @param $limit
+     * @param $offset
+     * @return string
+     */
     private function _buildLimit($limit, $offset)
     {
         if ($limit)
