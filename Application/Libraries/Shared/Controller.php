ce421a9120ea6a65e7b5545b579e04179af94ffa
Tom Wilford <hello@jollyblueman.com>
2021-05-16 22:36:07 +0100

    Added user to shared application controller

--- Application/Libraries/Shared/Controller.php
+++ Application/Libraries/Shared/Controller.php
@@ -4,11 +4,39 @@ namespace Shared;
 
 class Controller extends \Framework\Controller
 {
+    /**
+     * @var $_user
+     * @readwrite
+     */
+    protected $_user;
+
     public function __construct($options = [])
     {
         parent::__construct($options);
 
         $database = \Framework\Registry::get("database");
         $database->connect();
+
+        $session = \Framework\Registry::get("session");
+        $user    = unserialize($session->get("user", null));
+        $this->setUser($user);
+    }
+
+    public function render()
+    {
+        if ($this->getUser())
+        {
+            if ($this->getActionView())
+            {
+                $this->getActionView()->set("user", $this->getUser());
+            }
+
+            if ($this->getLayoutView())
+            {
+                $this->getLayoutView()->set("user", $this->getUser());
+            }
+        }
+
+        parent::render();
     }
 }
